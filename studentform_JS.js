65a1427200e720129a8fe444ba47d7f1b7f535b0
suganth81 <86565118+suganth81@users.noreply.github.com>
2021-07-08 22:44:48 +0530

    Add files via upload

--- studentform_JS.js
+++ studentform_JS.js
@@ -2,6 +2,7 @@ const usernameEl = document.querySelector('#username');
 const emailEl = document.querySelector('#email');
 const passwordEl = document.querySelector('#password');
 const confirmPasswordEl = document.querySelector('#confirm-password');
+const phonenumberEl = document.querySelector('#phonenumber');
 
 const form = document.querySelector('#signup');
 
@@ -23,9 +24,25 @@ const checkUsername = () => {
         showSuccess(usernameEl);
         valid = true;
     }
+    console.log(123);
     return valid;
 };
 
+
+const checkphonenumber = () => {
+    let valid = false;
+    const phonenumber = phonenumberEl.value.trim();
+          if (!isRequired(phonenumber)) {
+                showError(phonenumberEl, 'Phonenumber cannot be blank.');
+            } 
+            else {
+                showSuccess(phonenumberEl);
+                valid = true;
+            }
+            
+    return valid;
+    
+};
 
 
 
@@ -128,15 +145,42 @@ form.addEventListener('submit', function (e) {
     let isUsernameValid = checkUsername(),
         isEmailValid = checkEmail(),
         isPasswordValid = checkPassword(),
-        isConfirmPasswordValid = checkConfirmPassword();
+        isConfirmPasswordValid = checkConfirmPassword(),
+        isphonenumberValid = checkphonenumber();
 
     let isFormValid = isUsernameValid &&
         isEmailValid &&
         isPasswordValid &&
-        isConfirmPasswordValid;
+        isConfirmPasswordValid &&
+        isphonenumberValid;
 
     // submit to the server if the form is valid
     if (isFormValid) {
+        // Get the modal
+        var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
 
     }
 });
@@ -166,9 +210,11 @@ form.addEventListener('input', debounce(function (e) {
             break;
         case 'password':
             checkPassword();
-            break;
-        case 'confirm-password':
-            checkConfirmPassword();
+            break;        
+        case 'phonenumber':
+            checkphonenumber();
             break;
     }
+
+ 
 }));
